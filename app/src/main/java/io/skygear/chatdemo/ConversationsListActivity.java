e7b1b7c73a30319ffbe8b16e4414bb5b7b58b216
Carmen <carmen@oursky.com>
2018-02-05 20:10:08 +0800

    Add logout button

--- app/src/main/java/io/skygear/chatdemo/ConversationsListActivity.java
+++ app/src/main/java/io/skygear/chatdemo/ConversationsListActivity.java
@@ -19,6 +19,7 @@ import io.skygear.plugins.chat.Conversation;
 import io.skygear.plugins.chat.GetCallback;
 import io.skygear.skygear.Container;
 import io.skygear.skygear.Error;
+import io.skygear.skygear.LogoutResponseHandler;
 
 /**
  * Created by carmenlau on 10/17/17.
@@ -73,8 +74,25 @@ public class ConversationsListActivity extends AppCompatActivity {
             case R.id.users_list:
                 startActivity(new Intent(this, UsersListActivity.class));
                 return true;
+            case R.id.log_out:
+                logout();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    void logout() {
+        mSkygear.getAuth().logout(new LogoutResponseHandler() {
+            @Override
+            public void onLogoutSuccess() {
+                finish();
+            }
+
+            @Override
+            public void onLogoutFail(Error error) {
+                Toast.makeText(getBaseContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
